ff051f352a34d28b8d54cb96605b7b5f1bb33270
Darwin Ramos <2014110527@ub.edu.bz>
2025-04-19 18:18:14 -0600

    table

--- database/db.sql
+++ database/db.sql
@@ -24,3 +24,17 @@ CREATE TABLE Sender (
             FOREIGN KEY (sender_id) REFERENCES Sender(sender_id),
             FOREIGN KEY (receiver_id) REFERENCES Receiver(receiver_id)
         );
+
+        CREATE TABLE Packages (
+    tracking_number SERIAL PRIMARY KEY,
+    sender_name VARCHAR(100),
+    sender_address VARCHAR(255),
+    receiver_name VARCHAR(100),
+    receiver_address VARCHAR(255),
+    weight DECIMAL(10, 2),
+    cost_per_unit DECIMAL(10, 2),
+    shipping_method VARCHAR(50),
+    flat_fee DECIMAL(10, 2),
+    status VARCHAR(50),
+    type VARCHAR(50) -- e.g., 'One-Day', 'Two-Day'
+);
